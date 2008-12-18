0598cbf151d0e83b113d81f6c0cd518ada0f08fa
inaruta <mimitako@gmail.com>
2008-12-18 22:26:09 +0900

    Changed loggeing frequency to daily

--- lib/follotter_logger.rb
+++ lib/follotter_logger.rb
@@ -3,9 +3,9 @@ require 'logger'
 module FollotterLogger
   def log
     unless defined? $_log
-      path = File.dirname(__FILE__)+"/../log/crawler#{$$}.log"
+      path = File.dirname(__FILE__)+"/../log/crawler.log"
       puts path
-      $_log = Logger.new(path)
+      $_log = Logger.new(path, 'daily')
       $_log.level = Logger::INFO
     end
     $_log
